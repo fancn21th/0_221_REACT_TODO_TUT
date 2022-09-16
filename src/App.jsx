ec3a4a63bc45799087d9e65cd83e6c643acf104c
fancn21th <fancn21th@gmail.com>
2022-09-16 15:27:05 +0800

    step1: ui

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,27 @@ function App() {
   return (
     <div className="App">
       <h1>TODO List</h1>
+      <input />
+      <ul>
+        <li>吃饭</li>
+        <li>睡觉</li>
+        <li>打游戏</li>
+      </ul>
+
+      <ul className="command-list">
+        <li>
+          <h3>过滤：</h3>
+        </li>
+        <li>
+          <a href="#">全部</a>
+        </li>
+        <li>
+          <a href="#">完成</a>
+        </li>
+        <li>
+          <a href="#">未完成</a>
+        </li>
+      </ul>
     </div>
   );
 }
